75f1ff698e05152dc022e0721db082075c325da9
raymond <raymondan1031@gmail.com>
2022-09-26 19:30:55 +0800

    jsonify and check keys(if not all)

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,4 @@
-from crypt import methods
-from flask import Flask, redirect, request
+from flask import Flask, redirect, request, jsonify
 
 app = Flask(__name__)
 
@@ -15,9 +14,28 @@ def second_script():
 def youtube():
     return redirect("https://www.bilibili.com/")
 
-# @app.route("test/post", methods = ['POST'])
-# def first_post():
-#     my_json = request.get_json()
+@app.route("/test/post", methods = ['POST'])
+def first_post():
+    my_json = request.get_json()
+    print(my_json)
+
+    get_name = my_json.get("name")
+    get_age = my_json.get("age")
+
+    return "the name is {}, the age is {}".format(get_name, str(get_age))
+
+
+@app.route("/test/post2", methods = ['POST'])
+def json_post():
+    my_json = request.get_json()
+    print(my_json)
+
+    get_name = my_json.get("name")
+    get_age = my_json.get("age")
+    if not all([get_name, get_age]):
+        return jsonify(msg = "lacks of parameter")
+
+    return jsonify(name=get_name, age=get_age)
 
 app.run(host='0.0.0.0', port='7777')
 
