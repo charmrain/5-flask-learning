99901f5e9e7de3d08c2a51358a45a2e01d7ff5e8
raymond <raymondan1031@gmail.com>
2022-09-27 00:30:21 +0800

    create db

--- learn_sql.py
+++ learn_sql.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlit:///' + '/Users/anrui/Desktop/database/test.db'
 # the content after + is the location of db
 
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = 'abc'
+
+db = SQLAlchemy(app)
